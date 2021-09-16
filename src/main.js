ea4174a89a80e547ac6592b4c36e6f1bf0dde754
power <1984431982@qq.com>
2021-09-16 22:53:27 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -10,6 +10,18 @@ import './assets/css/global.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+/**
+ * 导入富文本编辑器
+ */
+import VueQuillEditor from 'vue-quill-editor'
+
+/**
+ * 导入富文本编辑器对应的样式
+ */
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -25,6 +37,42 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+/**
+ * 全局注册富文本编辑器 注册为全局可用组件
+ */
+Vue.use(VueQuillEditor)
+
+/**
+ * 格式化时间过滤器
+ */
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  /**
+   * 年
+   * 月
+   * 日
+   */
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  /**
+   * 时
+   * 分
+   * 秒
+   */
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  /**
+   * 拼接成完整日期字符串
+   */
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
